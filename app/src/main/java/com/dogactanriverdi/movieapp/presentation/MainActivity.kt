6d64fc519da5c013edbdb859ee3f5833fb854bd1
Dogac Tanriverdi <d.tnrvrdi@proton.me>
2024-09-26 21:06:45 +0300

    Cancel the transparent status bar. Add EdgeToEdge and adjust the layout according to edgeToEdge

--- app/src/main/java/com/dogactanriverdi/movieapp/presentation/MainActivity.kt
+++ app/src/main/java/com/dogactanriverdi/movieapp/presentation/MainActivity.kt
@@ -2,7 +2,10 @@ package com.dogactanriverdi.movieapp.presentation
 
 import android.os.Bundle
 import android.view.WindowManager
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.dogactanriverdi.movieapp.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,6 +13,12 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
