72a822f872df8af8e916bcbbb0097cf1573bf34e
Dogac Tanriverdi <d.tnrvrdi@proton.me>
2024-09-27 16:32:22 +0300

    Add profile screen

---
+++ app/src/main/java/com/dogactanriverdi/movieapp/presentation/profile/ProfileFragment.kt
@@ -0,0 +1,7 @@
+package com.dogactanriverdi.movieapp.presentation.profile
+
+import androidx.fragment.app.Fragment
+import com.dogactanriverdi.movieapp.R
+
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
+}
