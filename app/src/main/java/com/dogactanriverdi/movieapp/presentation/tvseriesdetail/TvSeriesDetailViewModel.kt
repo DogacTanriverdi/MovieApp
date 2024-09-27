e5c560e115ff0e9be28577e886d2daa63df507cc
Dogac Tanriverdi <d.tnrvrdi@proton.me>
2024-09-27 11:58:34 +0300

    Add watch list functions to TvSeriesDetailViewModel

--- app/src/main/java/com/dogactanriverdi/movieapp/presentation/tvseriesdetail/TvSeriesDetailViewModel.kt
+++ app/src/main/java/com/dogactanriverdi/movieapp/presentation/tvseriesdetail/TvSeriesDetailViewModel.kt
@@ -3,19 +3,24 @@ package com.dogactanriverdi.movieapp.presentation.tvseriesdetail
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.dogactanriverdi.movieapp.common.Resource
+import com.dogactanriverdi.movieapp.data.source.local.model.WatchListEntity
 import com.dogactanriverdi.movieapp.domain.usecase.tvseries.TvSeriesUseCases
+import com.dogactanriverdi.movieapp.domain.usecase.watchlist.WatchListUseCases
 import com.dogactanriverdi.movieapp.presentation.tvseriesdetail.state.TvSeriesCreditsState
 import com.dogactanriverdi.movieapp.presentation.tvseriesdetail.state.TvSeriesDetailState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class TvSeriesDetailViewModel @Inject constructor(
-    private val tvSeriesUseCases: TvSeriesUseCases
+    private val tvSeriesUseCases: TvSeriesUseCases,
+    private val watchListUseCases: WatchListUseCases
 ) : ViewModel() {
 
     private val _tvSeriesDetailState = MutableStateFlow(TvSeriesDetailState())
@@ -23,6 +28,9 @@ class TvSeriesDetailViewModel @Inject constructor(
 
     private val _tvSeriesCreditsState = MutableStateFlow(TvSeriesCreditsState())
     val tvSeriesCreditsState: StateFlow<TvSeriesCreditsState> = _tvSeriesCreditsState
+
+    private val _watchListState = MutableStateFlow<List<WatchListEntity>>(emptyList())
+    val watchListState: StateFlow<List<WatchListEntity>> = _watchListState
 
     fun getTvSeriesDetail(seriesId: Int, language: String) {
         viewModelScope.launch {
@@ -84,5 +92,23 @@ class TvSeriesDetailViewModel @Inject constructor(
                 }
             }
         }
+    }
+
+    fun addToWatchList(watchListEntity: WatchListEntity) {
+        viewModelScope.launch {
+            watchListUseCases.addToWatchList(watchListEntity)
+        }
+    }
+
+    fun deleteFromWatchList(watchListEntity: WatchListEntity) {
+        viewModelScope.launch {
+            watchListUseCases.deleteFromWatchList(watchListEntity)
+        }
+    }
+
+    fun getAllWatchList() {
+        watchListUseCases.getAllWatchList().onEach { watchList ->
+            _watchListState.value = watchList
+        }.launchIn(viewModelScope)
     }
 }
