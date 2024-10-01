3f7ec442eb91214199b75faa8f414837325cf3d4
DogacTanriverdi <d.tnrvrdi@proton.me>
2024-10-01 16:28:44 +0300

    Change version code and export signed abb

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.dogactanriverdi.movieapp"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
